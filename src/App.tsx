b3d7bfee5df5bf693aa5b75bbf1504e60e447680
simoneclr <simone.clclr@gmail.com>
2022-12-05 15:14:59 +0100

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      <h1>FM Multistep Form</h1>
+    <div className="p-4 font-ubuntu">
+      <h1>
+        FM Multistep Form
+      </h1>
     </div>
   );
 }
