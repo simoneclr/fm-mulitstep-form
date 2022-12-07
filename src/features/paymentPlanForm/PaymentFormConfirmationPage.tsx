0c5162e73c56bbc1021a468e0b5b57a69abde7d0
simoneclr <simone.clclr@gmail.com>
2022-12-07 16:39:38 +0100

    Add mobile styles for PaymentFormConfirmationPage

--- src/features/paymentPlanForm/PaymentFormConfirmationPage.tsx
+++ src/features/paymentPlanForm/PaymentFormConfirmationPage.tsx
@@ -1,7 +1,19 @@
+import { ReactComponent as ThankYouIcon } from "../../assets/images/icon-thank-you.svg"
+
 function PaymentFormConfirmationPage() {
 	return (
-		<div>
-			Thank You!
+		<div className="flex flex-col items-center pt-8 pb-12 gap-2 text-center">
+
+			<ThankYouIcon className="h-12 mb-2"/>
+
+			<h2 className="text-marine-blue text-2xl font-bold">
+				Thank You!
+			</h2>
+
+			<p className="text-cool-gray">
+				Thanks for confirming your subscription! We hope you have fun using our platform. 
+				If you ever need support, please feel free to email us at support@loremgaming.com.
+			</p>
 		</div>
 	)
 }
